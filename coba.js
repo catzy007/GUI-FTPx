48ae381f772a53f35b242a0136710497fb9f740c
catzy007 <catzy.rais@gmail.com>
2019-10-01 02:10:38 +0700

    add condition to list only file after set date

--- coba.js
+++ coba.js
@@ -1,24 +1,33 @@
-  var Client = require('ftp');
+var day=29;
+var month=08;
+var year=2019;
+var DlDate = new Date(year,month,day);
+//console.log(DlDate);
 
-  var c = new Client();
-  c.on('ready', function() {
-    c.list(function(err, list) {
-      if (err) throw err;
-      //console.dir(list);
-      for (let i=0; i<list.length; i+=1) {
+var Client = require('ftp');
+
+var c = new Client();
+c.on('ready', function() {
+  c.list(function(err, list) {
+    if (err) throw err;
+    //console.dir(list);
+    for (let i=0; i<list.length; i+=1) {
+      //only list file that more than DlDate
+      if(list[i].date > DlDate){
         console.log(list[i].name, list[i].date);
-       }
-      c.end();
-    });
+      }
+    }
+  c.end();
   });
-  // connect to localhost:21 as anonymous with password 1234
-  c.connect({
-    host: "localhost",
-    port: 21,
-    user: "anonymous",
-    password: "1234",
-    debug: console.log
-  });
-  console.log(c);
+});
+// connect to localhost:21 as anonymous with password 1234
+c.connect({
+  host: "localhost",
+  port: 21,
+  user: "efi4",
+  password: "1033",
+  debug: console.log
+});
+//console.log(c);
 
 //https://github.com/mscdex/node-ftp
