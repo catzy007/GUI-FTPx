eea6d6883d89259309e1e90855bcd582f8b8dc08
catzy007 <catzy.rais@gmail.com>
2019-10-01 03:00:45 +0700

    show list to electron window

--- index.js
+++ index.js
@@ -35,3 +35,44 @@ app.on('activate', function () {
 ipcMain.on('app_version', (event) => {
   event.sender.send('app_version', { version: app.getVersion() });
 });
+
+
+
+// =========================================
+var day=29; var month=08; var year=2019;
+var DlDate = new Date(year,month,day);
+//console.log(DlDate);
+
+var Client = require('ftp');
+var c = new Client();
+c.on('ready', function() {
+  c.list(function(err, list) {
+    if (err) throw err;
+    //pass list to electron window
+    ipcMain.on('listData', (event) => {
+      event.sender.send('listData', { mylist: list });
+    });
+    //console.dir(list);
+    // for (let i=0; i<list.length; i+=1) {
+    //   //only list file that more than DlDate
+    //   if(list[i].date > DlDate){
+    //     console.log(list[i].name, list[i].date);
+    //     ipcMain.on('listData', (event) => {
+    //       event.sender.send('listData', { mylist: list });
+    //     });
+    //   }
+    // }
+  c.end();
+  });
+});
+// connect to localhost:21 as anonymous with password 1234
+c.connect({
+  host: "localhost",
+  port: 21,
+  user: "efi4",
+  password: "1033",
+  debug: console.log
+});
+//console.log(c);
+
+//https://github.com/mscdex/node-ftp
