9130610f4f2b1f03ac384605db8c3e3a069a01a9
catzy007 <catzy.rais@gmail.com>
2019-10-03 18:33:34 +0700

    jajal

--- index.js
+++ index.js
@@ -38,6 +38,12 @@ ipcMain.on('app_version', (event) => {
 });
 
 
+//=============================================
+var serverAddr = "localhost";
+var serverPort = "21";
+var usernameFtp = "efi4";
+var passwordFtp = "1033";
+
 
 //=============================================
 function getData () {
@@ -47,21 +53,46 @@ function getData () {
     c.on('ready', function() {
       c.list(function(err, list) {
         if (err) reject(err); //reject promise
-        console.log(list);
+        //console.log(list);
         resolve(list) //resolve promise
       c.end();
       });
     });
     // connect to localhost:21 as efi4 with password 1033
     c.connect({
-      host: "localhost",
-      port: 21,
-      user: "efi4",
-      password: "1033",
+      host: serverAddr,
+      port: serverPort,
+      user: usernameFtp,
+      password: passwordFtp,
       //debug: console.log
     });
   });
 }
+
+//==================================
+function getFile(){
+  return new Promise((resolve, reject) => {
+    var Client = require('ftp');
+    var fs = require('fs');
+
+    var c = new Client();
+    c.on('ready', function() {
+      c.put('2.jpeg', '/home/efi4/2x.jpeg', function(err) {
+        if (err) reject(err);
+        resolve("done!");
+        c.end();
+      });
+    });
+    // connect to localhost:21 as anonymous
+    c.connect({
+      host: serverAddr,
+      port: serverPort,
+      user: usernameFtp,
+      password: passwordFtp,
+    });
+  });
+}
+
 
 
 //pass list data to electron window
@@ -71,8 +102,11 @@ ipcMain.on('listData', (event) => {
   }).catch((err) => {
     event.sender.send('errorData', { error: err });
   })
+
+  getFile().then((out) => {
+    console.log(out);
+  }).catch((err) => {
+    console.log(err);
+  })
 });
-
-//console.log(c);
-
 //https://github.com/mscdex/node-ftp
