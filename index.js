bd9ad7b86f609df40d11072cf0f864284eacef10
catzy <catzy.rais@gmail.com>
2019-10-04 01:36:42 +0700

    add sync message cpasser

--- index.js
+++ index.js
@@ -38,88 +38,29 @@ ipcMain.on('app_version', (event) => {
 
 
 //=============================================
-var serverAddr = "localhost";
-var serverPort = "21";
-var usernameFtp = "admin";
-var passwordFtp = "1234";
-
-ipcMain.on('arrInput',(event, arg) => {
+var data;
+ipcMain.on('arrMsg',(event, arg) => {
   console.log('index ' + arg);
-  serverAddr = arg[0];
-  serverPort = arg[1];
-  usernameFtp = arg[2];
-  passwordFtp = arg[3];
-
-  function getData () {
-    return new Promise((resolve, reject) => {
-      var Client = require('ftp');
-      var c = new Client();
-      c.on('ready', function() {
-        c.list(function(err, list) {
-          if (err) reject(err); //reject promise
-          console.log(list);
-          resolve(list) //resolve promise
-        c.end();
-        });
-      });
-      // connect to server
-      c.connect({
-        host: serverAddr,
-        port: serverPort,
-        user: usernameFtp,
-        password: passwordFtp,
-        //debug: console.log
-      });
+  var Client = require('ftp');
+  var c = new Client();
+  c.on('ready', function() {
+    c.list(function(err, list) {
+      if (err) throw (err);
+      //console.log(list);
+      event.returnValue = list
+      console.log(list[0].name);
+    c.end();
     });
-  }
-
-  //pass list data to electron window
-  ipcMain.on('listData', (event) => {
-    getData().then((list) => {    
-      event.sender.send('listData', { mylist: list });
-    }).catch((err) => {
-      event.sender.send('errorData', { error: err });
-    });
-
-    // getFile().then((out) => {
-    //   console.log(out);
-    // }).catch((err) => {
-    //   console.log(err);
-    // })
+  });
+  // connect to server
+  c.connect({
+    host: arg[0],
+    port: arg[1],
+    user: arg[2],
+    password: arg[3],
+    //debug: console.log
   });
 });
-
-
-
-//=============================================
-
-
-//=============================================
-// function getFile(){
-//   return new Promise((resolve, reject) => {
-//     var Client = require('ftp');
-//     var fs = require('fs');
-
-//     var c = new Client();
-//     c.on('ready', function() {
-//       c.put('2.jpeg', '/home/efi4/2x.jpeg', function(err) {
-//         if (err) reject(err);
-//         resolve("done!");
-//         c.end();
-//       });
-//     });
-//     // connect to localhost:21 as anonymous
-//     c.connect({
-//       host: serverAddr,
-//       port: serverPort,
-//       user: usernameFtp,
-//       password: passwordFtp,
-//     });
-//   });
-// }
-
-
-
 
 //https://github.com/mscdex/node-ftp
 
